6f67a37f65a537247bba493214339933ff1c64e8
chris bording <chris.bording@uwa.edu.au>
2024-02-16 01:53:46 +0000

    add bio-blast.sh

---
+++ bio-blast.sh
@@ -0,0 +1,11 @@
+#!/bin/bash  
+
+
+   makeblastdb -in zebrafish.1.protein.faa -dbtype prot
+   blastp -query mm-first.fa -db zebrafish.1.protein.faa
+   blastp -query mm-first.fa -db zebrafish.1.protein.faa -out mm-first.x.zebrafsih.txt 
+ 
+   head -498 mouse.1.protein.faa > mm-second.fa
+   blastp -query mm-second.fa -db zebrafish.1.protein.faa -out mm-second.x.zebrafish.txt 
+   blastp -query mm-second.fa -db zebrafish.1.protein.faa -out mm-second.x.zebrafish.tsv -outfmt 6 
+ 
